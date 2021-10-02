3563b3e2b7f826141bd8abb75377273266a9937b
Quinn Larson <quinn@ramblehousegames.com>
2021-10-03 08:31:45 +0900

    Wavy Pagoda

--- Assets/Scripts/Character2.cs
+++ Assets/Scripts/Character2.cs
@@ -31,11 +31,28 @@ public class Character2 : MonoBehaviour
     [SerializeField]
     private float shurikenSpawnOffset = 1f;
 
+    [SerializeField]
+    private GameObject pagoda;
+
+    private bool amGrounded = false;
+
     // Update is called once per frame
     void Update()
     {
+        Collider2D ground = GetGround();
+        if(amGrounded && ground == null)
+        {
+            transform.SetParent(pagoda.transform);
+            amGrounded = false;
+        }
+        else if(!amGrounded && ground != null)
+        {
+            transform.SetParent(ground.transform);
+            amGrounded = true;
+        }
+
         myRigidbody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), myRigidbody.velocity.y);
-        if(AmGrounded() && Input.GetButtonDown("Jump"))
+        if(amGrounded && Input.GetButtonDown("Jump"))
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpSpeed);
 
         if (myRenderer.flipX && Input.GetAxis("Horizontal") > 0)
@@ -43,7 +60,7 @@ public class Character2 : MonoBehaviour
         else if (!myRenderer.flipX && Input.GetAxis("Horizontal") < 0)
             myRenderer.flipX = true;
 
-        myAnimator.SetBool("Grounded", AmGrounded());
+        myAnimator.SetBool("Grounded", amGrounded);
         myAnimator.SetBool("LateralMovement", Mathf.Abs(Input.GetAxis("Horizontal")) > .1f);
 
         if (Input.GetButtonDown("Fire1"))
@@ -66,9 +83,9 @@ public class Character2 : MonoBehaviour
         }
     }
 
-    private bool AmGrounded()
+    private Collider2D GetGround()
     {
         RaycastHit2D hit = Physics2D.Raycast(myCollider.bounds.center, Vector2.down, myCollider.bounds.extents.y + .1f, groundLayerMask);
-        return hit.collider != null;
+        return hit.collider;
     }
 }
--- Assets/Scripts/Pagoda.cs
+++ Assets/Scripts/Pagoda.cs
@@ -12,4 +12,19 @@ public class Pagoda : MonoBehaviour
 
     [SerializeField]
     private List<PagodaRow> rows;
+
+    private float waveProgress = 0f;
+    private float waveAmplitude = 1f;
+    private float waveSpeed = .1f;
+
+    private void Update()
+    {
+        for (int i = 0; i < rows.Count; i++)
+        {
+            waveProgress += Time.deltaTime * waveSpeed;
+            float offset = waveProgress + (i / waveAmplitude);
+            rows[i].transform.position = new Vector3((Mathf.Sin(offset)) * waveAmplitude, rows[i].transform.position.y, rows[i].transform.position.z);
+
+        }
+    }
 }
